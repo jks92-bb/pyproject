c0204baccad61e81dc1ba99449b951dc9e52d953
jks92-bb <obhop92@gmail.com>
2024-02-19 10:52:19 +0900

    update

--- weather_fashion_chatbot_ver1.5/maple_util.py
+++ weather_fashion_chatbot_ver1.5/maple_util.py
@@ -7,7 +7,7 @@ def get_yesterday_date():
 
 def get_character_data(character_name):
     headers = {
-        "x-nxopen-api-key": "test_e6f5684cbfb6237d5d3ff8740d5ee712aeb8ddd9ba8877df5e1d291949e7e6a280553df27fa82c9e2a239d36cabd158b"
+        "x-nxopen-api-key": "키값"
     }
 
     url_string = f"https://open.api.nexon.com/maplestory/v1/id?character_name={character_name}"
@@ -22,7 +22,7 @@ def get_character_data(character_name):
 
 def get_character_basic(ocid, date):
     headers = {
-        "x-nxopen-api-key": "test_e6f5684cbfb6237d5d3ff8740d5ee712aeb8ddd9ba8877df5e1d291949e7e6a280553df27fa82c9e2a239d36cabd158b"
+        "x-nxopen-api-key": "키값"
     }
 
     url_string = f"https://open.api.nexon.com/maplestory/v1/character/basic?ocid={ocid}&date={date}"
@@ -37,7 +37,7 @@ def get_character_basic(ocid, date):
 
 def get_character_stat(ocid, date):
     headers = {
-        "x-nxopen-api-key": "test_e6f5684cbfb6237d5d3ff8740d5ee712aeb8ddd9ba8877df5e1d291949e7e6a280553df27fa82c9e2a239d36cabd158b"
+        "x-nxopen-api-key": "키값"
     }
 
     url_string = f"https://open.api.nexon.com/maplestory/v1/character/stat?ocid={ocid}&date={date}"
